b995686b26b7c060aff72b593f391d61ea3c464e
Kiet Tran <17110165@student.hcmute.edu.vn>
2019-02-22 11:02:48 +0700

    Submit Bài tập 2

--- Bài Tập 2/Bài Tập 2/Form1.Designer.cs
+++ Bài Tập 2/Bài Tập 2/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnKhungHCN = new System.Windows.Forms.Button();
+            this.btnHCN = new System.Windows.Forms.Button();
+            this.btnKhungEllipse = new System.Windows.Forms.Button();
+            this.btnEllipse = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnDuongThang = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(158, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(642, 450);
+            this.pnlMain.TabIndex = 3;
+            this.pnlMain.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlMain_Paint);
+            this.pnlMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlMain_MouseDown);
+            this.pnlMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlMain_MouseMove);
+            this.pnlMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlMain_MouseUp);
+            // 
+            // btnKhungHCN
+            // 
+            this.btnKhungHCN.Location = new System.Drawing.Point(3, 58);
+            this.btnKhungHCN.Name = "btnKhungHCN";
+            this.btnKhungHCN.Size = new System.Drawing.Size(149, 49);
+            this.btnKhungHCN.TabIndex = 16;
+            this.btnKhungHCN.Text = "Khung Hình Chũ Nhật";
+            this.btnKhungHCN.UseVisualStyleBackColor = true;
+            // 
+            // btnHCN
+            // 
+            this.btnHCN.Location = new System.Drawing.Point(3, 113);
+            this.btnHCN.Name = "btnHCN";
+            this.btnHCN.Size = new System.Drawing.Size(149, 49);
+            this.btnHCN.TabIndex = 17;
+            this.btnHCN.Text = "Hình Chữ Nhật";
+            this.btnHCN.UseVisualStyleBackColor = true;
+            // 
+            // btnKhungEllipse
+            // 
+            this.btnKhungEllipse.Location = new System.Drawing.Point(3, 168);
+            this.btnKhungEllipse.Name = "btnKhungEllipse";
+            this.btnKhungEllipse.Size = new System.Drawing.Size(149, 49);
+            this.btnKhungEllipse.TabIndex = 18;
+            this.btnKhungEllipse.Text = "Khung hình Ellipse";
+            this.btnKhungEllipse.UseVisualStyleBackColor = true;
+            // 
+            // btnEllipse
+            // 
+            this.btnEllipse.Location = new System.Drawing.Point(3, 223);
+            this.btnEllipse.Name = "btnEllipse";
+            this.btnEllipse.Size = new System.Drawing.Size(149, 49);
+            this.btnEllipse.TabIndex = 19;
+            this.btnEllipse.Text = "Hình Ellipse";
+            this.btnEllipse.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Controls.Add(this.btnDuongThang);
+            this.flowLayoutPanel1.Controls.Add(this.btnKhungHCN);
+            this.flowLayoutPanel1.Controls.Add(this.btnHCN);
+            this.flowLayoutPanel1.Controls.Add(this.btnKhungEllipse);
+            this.flowLayoutPanel1.Controls.Add(this.btnEllipse);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(158, 450);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btnDuongThang
+            // 
+            this.btnDuongThang.Location = new System.Drawing.Point(3, 3);
+            this.btnDuongThang.Name = "btnDuongThang";
+            this.btnDuongThang.Size = new System.Drawing.Size(149, 49);
+            this.btnDuongThang.TabIndex = 15;
+            this.btnDuongThang.Text = "Đường Thẳng";
+            this.btnDuongThang.UseVisualStyleBackColor = true;
+            this.btnDuongThang.Click += new System.EventHandler(this.btnDuongThang_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnKhungHCN;
+        private System.Windows.Forms.Button btnHCN;
+        private System.Windows.Forms.Button btnKhungEllipse;
+        private System.Windows.Forms.Button btnEllipse;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnDuongThang;
     }
 }
 
--- Bài Tập 2/Bài Tập 2/Form1.cs
+++ Bài Tập 2/Bài Tập 2/Form1.cs
@@ -12,9 +12,89 @@ namespace Bài_Tập_2
 {
     public partial class Form1 : Form
     {
+        Graphics gp;
+        bool bLine = false;
+
+        Pen myPen;
+        Color myColor;
+
+        bool isStart = false;   //Cờ xác định đi vẽ
+
+        List<cLine> lLine = new List<cLine>();
+
         public Form1()
         {
             InitializeComponent();
+
+            gp = this.pnlMain.CreateGraphics();
+            myColor = Color.Blue;
+            myPen = new Pen(myColor);
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.isStart = true;        //bắt đầu vẽ
+
+            if (bLine == true)
+            {
+                cLine myLine = new cLine();
+                myLine.p1 = e.Location;
+                myLine.p2 = e.Location;
+                this.lLine.Add(myLine);
+            }
+        }
+
+        private void pnlMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.isStart == true)
+            {
+                if (this.bLine == true)
+                {
+                    //Cập nhật lại vị trí con chuột hiện tại
+                    this.lLine[this.lLine.Count - 1].p2 = e.Location;
+                    this.pnlMain.Refresh(); //Làm tười màn hình
+                    //Xóa sách màn hình và gọi hàm Paint
+                }
+            }
+        }
+
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+            if (this.isStart == true)
+            {
+                for (int i = 0; i < this.lLine.Count; i++)
+                {
+                    gp.DrawLine(myPen, lLine[i].p1, lLine[i].p2);
+                }
+            }
+        }
+
+        private void pnlMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (this.bLine == true)
+            {
+                this.lLine[this.lLine.Count - 1].p2 = e.Location;
+                this.pnlMain.Refresh();
+            }
+            this.isStart = false;
+            this.bLine = false;
+        }
+
+        private void btnDuongThang_Click(object sender, EventArgs e)
+        {
+            bLine = true;
+        }
+    }
+
+    public class cLine
+    {
+        public Point p1;
+        public Point p2;
+       
     }
 }
